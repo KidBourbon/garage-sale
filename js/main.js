0bfceae73bd094763160496d1dda8c7b1c983c52
KidBourbon <kidbourbongamer@gmail.com>
2023-08-14 01:39:32 -0400

    Merged index.html and mobile-menu.html with JS. Some CSS improvements

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const mobileMenuIcon = document.querySelector('.mobile-menu-icon');
 
 navbarEmail.addEventListener('click', toogleDesktopMenu);
+mobileMenuIcon.addEventListener('click', toogleMobileMenu);
 
 function toogleDesktopMenu() {
 	desktopMenu.classList.toggle('inactive');
+}
+
+function toogleMobileMenu() {
+	mobileMenu.classList.toggle('inactive');
 }
